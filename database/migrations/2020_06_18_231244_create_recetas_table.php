aac9e7538b3c5752072f3eb9f618a14f186193fe
JoseCortez1 <jose.cucei96@gmail.com>
2020-06-20 02:17:29 -0500

    added schema for categorias_recetas and added change on recetas

--- database/migrations/2020_06_18_231244_create_recetas_table.php
+++ database/migrations/2020_06_18_231244_create_recetas_table.php
@@ -13,10 +13,26 @@ class CreateRecetasTable extends Migration
      */
     public function up()
     {
+        Schema::create('categorias_recetas', function ( Blueprint $table){
+            $table->id();
+            $table->timestamps();
+            $table->string('categoria')->comment('el nombre de la categoria');
+
+
+
+        });
+
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('titulo');
+            $table->text('ingredientes');
+            $table->text('preparacion');
+            $table->string('imagen');
+            $table->foreignId('user_id')->references('id')->on('users')->comment('Llave foranea de user');
+            $table->foreignId('categoria_id')->index('id')->on('categorias_recetas')->comment('La categoria de la receta');
+
         });
     }
 
@@ -27,6 +43,7 @@ class CreateRecetasTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('categorias_recetas');
         Schema::dropIfExists('recetas');
     }
 }
